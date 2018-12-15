efa30c264a82989e2ff0757b9cc3832929e2697d
ignacioPastor <ignacio.pastor@clearpeaks.com>
2018-12-15 19:25:58 +0100

    stable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MyLibraryModule } from 'my-library';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MyLibraryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
